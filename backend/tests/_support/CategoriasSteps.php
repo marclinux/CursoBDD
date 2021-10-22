679537824cbe5075c5d1d706cb0918b3c68e0156
Marcos Hernandez <marclinux@yahoo.com>
2021-10-22 00:16:22 -0500

    Agregue el feature y los steps de categorias

---
+++ backend/tests/_support/CategoriasSteps.php
@@ -0,0 +1,112 @@
+<?php
+namespace backend\tests;
+
+use Codeception\Util\Locator;
+use yii\helpers\Url;
+
+class CategoriasSteps
+{
+    protected $I;
+    public function __construct(AcceptanceTester $I)
+    {
+        $this->I = $I;
+    }
+
+    /**
+     * @Given que estoy en la pagina de crear categoria
+     */
+     public function queEstoyEnLaPaginaDeCrearCategoria()
+     {
+         $this->I->amOnPage(Url::toRoute('/categorias/creatr'));
+     }
+
+    /**
+     * @Given capturo :arg1 en el dato de la clave
+     */
+     public function capturoEnElDatoDeLaClave($arg1)
+     {
+         $this->I->fillField('#categorias-clavecategoria', $arg1);
+     }
+
+    /**
+     * @Given capturo :arg1 en el dato de la descripcion
+     */
+     public function capturoEnElDatoDeLaDescripcion($arg1)
+     {
+        $this->I->fillField('#categorias-nombrecategoria', $arg1);
+     }
+
+    /**
+     * @Given selecciono :arg1 en el dato de categoria padre
+     */
+     public function seleccionoEnElDatoDeCategoriaPadre($arg1)
+     {
+         $this->I->selectOption('select', $arg1);
+     }
+
+    /**
+     * @When doy click en :arg1
+     */
+     public function doyClickEn($arg1)
+     {
+         $this->I->click($arg1);
+     }
+
+    /**
+     * @Then debo tener una categoria con los dato de :arg1 en la clave, :arg2 en la descripcion y :arg3 en la categoria padre
+     */
+     public function deboTenerUnaCategoriaConLosDatoDeEnLaClaveEnLaDescripcionYEnLaCategoriaPadre($arg1, $arg2, $arg3)
+     {
+        $this->I->seeInDatabase('categorias', ['ClaveCategoria' => $arg1, 'NombreCategoria' => $arg2, 'idCategoriaPadre' => $arg3]);         
+     }
+
+    /**
+     * @Given que estoy en la pagina de modificar categoria con el id :arg1
+     */
+     public function queEstoyEnLaPaginaDeModificarCategoriaConElId($arg1)
+     {
+         $ruta = '/categorias/update?id=' . $arg1;
+         $this->I->amOnPage(Url::toRoute($ruta));
+     }
+
+    /**
+     * @Then debo tener los datos de :arg1 en la clave, :arg2 en la descripcion de la categoria con id :arg3  
+     */
+     public function deboTenerLosDatosDeEnLaClaveEnLaDescripcionDeLaCategoriaConId($arg1, $arg2, $arg3)       
+     {
+        $this->I->seeInDatabase('categorias', ['ClaveCategoria' => $arg1, 'NombreCategoria' => $arg2, 'idCategoriaPadre' => $arg3]);
+     }
+
+    /**
+     * @Given que estoy en la pagina de mostrar categorias
+     */
+     public function queEstoyEnLaPaginaDeMostrarCategorias()
+     {
+         $this->I->amOnPage(Url::toRoute('/categorias'));
+     }
+
+    /**
+     * @When doy click en el icono de eliminar de la categoria con id :arg1
+     */
+     public function doyClickEnElIconoDeEliminarDeLaCategoriaConId($arg1)
+     {
+         $selectorUrl = '/proyectosYii2/ExRestaurantBDD/backend/web/index-test.php/categorias/delete?id=' . $arg1;
+         $this->I->click(Locator::href($selectorUrl));
+     }
+
+    /**
+     * @When confirmo eliminar el registro
+     */
+     public function confirmoEliminarElRegistro()
+     {
+        $this->I->click('Si');
+     }
+
+    /**
+     * @Then la categoria con clave :arg1 yo no debe existir
+     */
+     public function laCategoriaConClaveYoNoDebeExistir($arg1)
+     {
+         $this->I->dontSeeInDatabase('categorias', ['ClaveCategoria' => $arg1]);
+     }
+}
---
+++ backend/tests/acceptance/Categorias.feature
@@ -0,0 +1,27 @@
+#language: es
+Característica: Categorias
+  En orden para permitir administras las categorias
+  Como un usuario administrador
+  Yo necesito dar de alta, modificar y eliminar categorias
+
+  Escenario: Agregar una categoria
+  Dado que estoy en la pagina de crear categoria
+  Y capturo "C001" en el dato de la clave
+  Y capturo "CARNES" en el dato de la descripcion
+  Y selecciono "--sin categoria padre" en el dato de categoria padre
+  Cuando doy click en "Save"
+  Entonces debo tener una categoria con los dato de "C001" en la clave, "CARNES" en la descripcion y "0" en la categoria padre
+
+  Escenario: Modificar una categoria
+  Dado que estoy en la pagina de modificar categoria con el id "1"
+  Y capturo "C002" en el dato de la clave
+  Y capturo "SOPAS" en el dato de la descripcion
+  Y selecciono "--sin categoria padre" en el dato de categoria padre
+  Cuando doy click en "Save"
+  Entonces debo tener los datos de "C002" en la clave, "SOPAS" en la descripcion de la categoria con id "1"
+
+  Escenario: Eliminar una categoria
+  Dado que estoy en la pagina de mostrar categorias
+  Cuando doy click en el icono de eliminar de la categoria con id "1"
+  Y confirmo eliminar el registro
+  Entonces la categoria con clave "C002" yo no debe existir
